53d520efd915f7ec9f5fa31576cc3ac56a83dcc6
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2015-06-06 17:23:10 +0900

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,62 +2,34 @@ require 'spec_helper'
 
 describe "Static pages(静的ページの特徴)" do
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
 
   describe "Home page(ホームページの特徴)" do
+    before { visit root_path }
 
-    it "should have the content 'Sample App'(sample_appという文字列があるか)" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the right title (タイトルがRuby on Rails Tutorialであるか)" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
-    end
-
-    it "should not have a custom page title (カスタムしたページタイトルになっているべき)" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title("| Home")
-    end
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
-  describe "Help page(Helpページの特徴)" do
+  describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'(Helpという文字列があるか)" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the right title (タイトルが'Help'であるか)" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
-  describe "About page(Aboutページの特徴)" do
+  describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'About'(Aboutという文字列があるか)" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
+  end
 
-    it "should have the right title (タイトルが'About Us'であるか)" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Us")
-    end
-  end  
+  describe "Contact page" do
+    before { visit contact_path }
 
-  describe "Contact page(Contactページの特徴)" do
-
-    it "should have the content 'Contact'(Contactという文字列があるか)" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
-
-    it "should have the right title (タイトルが'Contact'であるか)" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
